6b791ab9484c002de4aaf7948eb940a3cc3addc9
M4thG33k <pepethemathgeek@gmail.com>
2016-04-16 08:50:27 -0500

    Added JEI integration for the Improved Crafting Table (The ICT block breaking and recipe conflicts were fixed in the last commit Fixed #1 )

---
+++ src/main/java/com/m4thg33k/lit/JEIIntegration/LitPlugin.java
@@ -0,0 +1,45 @@
+package com.m4thg33k.lit.JEIIntegration;
+
+import com.m4thg33k.lit.client.gui.GuiImprovedCraftingTable;
+import com.m4thg33k.lit.inventory.ContainerImprovedCraftingTable;
+import mezz.jei.api.*;
+import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
+import mezz.jei.api.recipe.transfer.IRecipeTransferRegistry;
+
+import javax.annotation.Nonnull;
+
+@JEIPlugin
+public class LitPlugin extends BlankModPlugin{
+    @Override
+    public void register(@Nonnull IModRegistry registry) {
+        IItemRegistry itemRegistry = registry.getItemRegistry();
+        IJeiHelpers jeiHelpers = registry.getJeiHelpers();
+
+        jeiHelpers.getNbtIgnoreList().ignoreNbtTagNames(
+                "AttributeModifiers",
+                "CanDestroy",
+                "CanPlaceOn",
+                "display",
+                "HideFlags",
+                "RepairCost",
+                "Unbreakable"
+        );
+
+        IGuiHelper guiHelper = jeiHelpers.getGuiHelper();
+        //registry.addRecipeCategories(new CraftingRecipeCategory(guiHelper));
+
+//        registry.addRecipeHandlers(
+//                new ShapedOreRecipeHandler(),
+//                new ShapedRecipesHandler(),
+//                new ShapelessOreRecipeHandler(),
+//                new ShapelessRecipesHandler());
+
+        registry.addRecipeClickArea(GuiImprovedCraftingTable.class,88,32,28,23, VanillaRecipeCategoryUid.CRAFTING);
+
+        IRecipeTransferRegistry recipeTransferRegistry = registry.getRecipeTransferRegistry();
+
+        recipeTransferRegistry.addRecipeTransferHandler(ContainerImprovedCraftingTable.class,VanillaRecipeCategoryUid.CRAFTING,1,9,10,36);
+
+//        registry.addRecipes(CraftingManager.getInstance().getRecipeList());
+    }
+}
