2bf1ac8161eb6cbe3ba98b91df638748f870c79a
M4thG33k <pepethemathgeek@gmail.com>
2016-04-16 08:13:08 -0500

    fixed ICT not breaking correctly and a config for its recipe

--- src/main/java/com/m4thg33k/lit/core/init/ModRecipes.java
+++ src/main/java/com/m4thg33k/lit/core/init/ModRecipes.java
@@ -3,6 +3,7 @@ package com.m4thg33k.lit.core.init;
 import com.m4thg33k.lit.api.chest.ChestTypes;
 import com.m4thg33k.lit.api.furnace.FurnaceTypes;
 import com.m4thg33k.lit.blocks.ModBlocks;
+import com.m4thg33k.lit.lib.LITConfigs;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -15,7 +16,14 @@ public class ModRecipes {
         FurnaceTypes.registerRecipes();
         ChestTypes.regRecipes();
 
-        GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.improvedCraftingTableBlock,1),new ItemStack(Blocks.crafting_table,1));
-        GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.improvedCraftingTableBlock,1),new ItemStack(Blocks.crafting_table,1),new ItemStack(Items.flint,1)); //just in case of recipe conflicts
+        if (LITConfigs.USE_ALTERNATE_CRAFTING_TABLE_RECIPE)
+        {
+            GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.improvedCraftingTableBlock,1),new ItemStack(Blocks.crafting_table,1),new ItemStack(Items.flint,1)); //just in case of recipe conflicts
+        }
+        else
+        {
+            GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.improvedCraftingTableBlock,1),new ItemStack(Blocks.crafting_table,1));
+        }
+
     }
 }
